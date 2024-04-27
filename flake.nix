70407a507611e084fbea07304657532f7528abd6
Kris Nuttycombe <kris@nutty.land>
2024-04-27 15:44:16 -0600

    Update to nixpkgs 23.05
    It is currently not possible to update to 23.11 because `true-name` has
    template-haskell 2.20 as an upper bound, and 23.11 supplies
    template-haskell 2.21.

--- flake.nix
+++ flake.nix
@@ -4,14 +4,12 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/release-23.05";
     flake-utils.url = "github:numtide/flake-utils";
-    true-name.url = "github:nuttycom/true-name/55b85e5d3b1e58fe97bce28e541edea4cfde9772";
   };
 
   outputs = {
     self,
     nixpkgs,
     flake-utils,
-    true-name,
   }: let
     pkg-name = "thyme";
     haskell-overlay = hfinal: hprev: {
@@ -23,13 +21,13 @@
     };
   in {
       overlays = {
-        default = nixpkgs.lib.composeExtensions true-name.overlays.default overlay;
+        default = overlay;
       };
     } //
     flake-utils.lib.eachDefaultSystem (system: let
       pkgs = import nixpkgs {
         inherit system;
-        overlays = [true-name.overlays.default overlay];
+        overlays = [overlay];
       };
 
       hspkgs = pkgs.haskellPackages;
